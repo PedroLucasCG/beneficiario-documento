65a6a101e7c43a53af019ebd9fc38a6de9a99c36
PedroLucasCG <pedrowwww44@gmail.com>
2026-07-14 16:29:35 -0300

    commit inicial

---
+++ src/main/java/com/wakanda/funcionario_documento/FuncionarioDocumentoApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda.funcionario_documento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioDocumentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionarioDocumentoApplication.class, args);
+	}
+
+}
