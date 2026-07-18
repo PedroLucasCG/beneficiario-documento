4f3921698b0bc4e977020b2844f1bceaf76547c7
PedroLucasCG <pedrowwww44@gmail.com>
2026-07-18 13:52:44 -0300

    fix: mudanca na configuracao da open api para passar token

--- src/main/java/com/wakanda/beneficiario_documento/config/openapi_docs/OpenApiConfig.java
+++ src/main/java/com/wakanda/beneficiario_documento/config/openapi_docs/OpenApiConfig.java
@@ -10,17 +10,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
                 .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                );
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
